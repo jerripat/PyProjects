2efc5bfa793da216185ccc1d03f371a452af45f7
jerripat <jerripatterson@gmail.com>
2022-10-15 21:56:19 -0500

    Update to program Enter file to update:Added CBoxs and new windows

--- NewWindows.py
+++ NewWindows.py
@@ -1,15 +1,21 @@
 from tkinter import *
-from tkinter import messagebox
-from tkinter import ttk
 
 root = Tk()
 root.title("New Windows")
 root.geometry("450x550")
 
 
+def newWindow():
+    new = Toplevel()
+    new.title("Second window Windows")
+    new.geometry("300x350")
+    my_label = Label(new, text="This is a new window").pack(pady=20)
+    new.mainloop()
 
 
-
+# Create new window
+my_button = Button(root, text="Open new window", command=newWindow)
+my_button.pack(pady=10)
 
 
 root.mainloop()
