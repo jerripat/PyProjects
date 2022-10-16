c4f344d7557355f3978b093f68a8748246fb19b3
jerripat <jerripatterson@gmail.com>
2022-10-16 15:21:40 -0500

    Update to program Enter file to update:New windows with pics

--- PyMenu/NewWindows.py
+++ PyMenu/NewWindows.py
@@ -1,21 +1,19 @@
 from tkinter import *
-#from PIL import ImageTk
-#from PIL import Image
-
+from PIL import ImageTk, Image
 
 root = Tk()
 root.title("New Windows")
-root.geometry("450x550")
+root.geometry("400x400")
 
 
 def newWindow():
     new = Toplevel()
     new.title("Second window Windows")
-    new.geometry("350x350")
+    new.geometry("400x500")
     my_label = Label(new, text="This is a new window").pack(pady=20)
 
-    #my_img = ImageTk.PhotoImage(Image.open("Images/EmpireState.jpg"))
-    #img_label = Label(new, image=my_img).pack(pady=10).pack(pady=5)
+    my_img = ImageTk.PhotoImage(Image.open("Images/EmpireState.jpg"))
+    img_label = Label(new, image=my_img).pack(pady=10)
 
     # Minimize original window
     #hide_button = Button(new, text="Hide Main Widow", command=root.iconify)
