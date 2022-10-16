a1c4dc5bf25acd45474f1768d606408335fe45bb
jerripat <jerripatterson@gmail.com>
2022-10-16 13:10:06 -0500

    Update to program Enter file to update:Update PyMenu File

--- PyMenu/NewWindows.py
+++ PyMenu/NewWindows.py
@@ -1,4 +1,7 @@
 from tkinter import *
+#from PIL import ImageTk
+#from PIL import Image
+
 
 root = Tk()
 root.title("New Windows")
@@ -8,8 +11,27 @@ root.geometry("450x550")
 def newWindow():
     new = Toplevel()
     new.title("Second window Windows")
-    new.geometry("300x350")
+    new.geometry("350x350")
     my_label = Label(new, text="This is a new window").pack(pady=20)
+
+    #my_img = ImageTk.PhotoImage(Image.open("Images/EmpireState.jpg"))
+    #img_label = Label(new, image=my_img).pack(pady=10).pack(pady=5)
+
+    # Minimize original window
+    #hide_button = Button(new, text="Hide Main Widow", command=root.iconify)
+    #show_button = Button(new, text="Show Main Window", command=root.deiconify)
+
+    # Withdraw original Window
+    hide_button = Button(new, text="Hide Main Widow", command=root.withdraw)
+    show_button = Button(new, text="Show Main Window", command=root.deiconify)
+    hide_button.pack()
+    show_button.pack()
+
+    kill_original = Button(new, text="Kill Original",
+                           command=root.destroy).pack(pady=10)
+    destroy_button = Button(
+        new, text="Quit", command=new.destroy).pack(pady=20)
+
     new.mainloop()
 
 
